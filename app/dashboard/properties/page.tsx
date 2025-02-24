91d40807f8b5c0ff9bf20c539c741d386e5929b3
brankodra <dragosavljevibr77238@th-nuernberg.de>
2025-02-24 17:50:55 +0100

    added rentalIncome_table, data-fetch and updated page

--- app/dashboard/properties/page.tsx
+++ app/dashboard/properties/page.tsx
@@ -1,5 +1,6 @@
 import Table from '@/app/ui/properties/table'
 import { Metadata } from 'next'
+import RentalIncomeTable from '@/app/ui/properties/rentalIncome-table'
 
 export const metadata: Metadata = {
   title: 'Properties'
@@ -8,8 +9,20 @@ export const metadata: Metadata = {
 export default async function Page() {
   return (
     <div>
-      <h1>Property Listing</h1>
-      <Table />
+      <div>
+        <h1>Property Listing</h1>
+        <Table />
+      </div>
+      <div>
+        <div className='mt-5'>
+          <h1>Outstanding Rental Payments</h1>
+          <RentalIncomeTable isPaid={false} />
+        </div>
+        <div className='mt-5'>
+          <h1>Received Rental Payments</h1>
+          <RentalIncomeTable isPaid={true} />
+        </div>
+      </div>
     </div>
   )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,3 +19,29 @@ export async function fetchProperties(token: any) {
     return []
   }
 }
+
+export async function fetchRentalIncome(token: any, isPaid: boolean) {
+  try {
+    const apiUrl = process.env.property_service_url
+    const res = await fetch(
+      apiUrl + `/properties/rental-income-summary/?isPaid=${isPaid}`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          Authorization: token ? `Bearer ${token}` : '',
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    if (!res.ok) {
+      throw new Error(`Failed to fetch properties: ${res.statusText}`)
+    }
+    const data = await res.json()
+    console.log(data.properties)
+    return data.properties
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
